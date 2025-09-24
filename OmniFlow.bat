96f76ecadbe73d0c66e908ff03f32c9f914248be
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2025-09-24 06:35:52 +0530

    Update OmniFlow.bat

--- OmniFlow.bat
+++ OmniFlow.bat
@@ -34,6 +34,15 @@ if not errorlevel 1 (
     )
 )
 
+REM Check for and process any .txt files left in the main folder
+dir /b *.txt >nul 2>&1
+if not errorlevel 1 (
+    for /f "delims=" %%T in ('dir /b *.txt') do (
+        set "txt_file=%%T"
+        goto :process_txt_files
+    )
+)
+
 REM This section is only reached when no .SAR files are found
 echo.
 echo All files have been processed. Waiting for new files.
@@ -86,6 +95,31 @@ goto file_process_loop
     echo.
     goto file_process_loop
 
+:process_txt_files
+    REM Extract the SNOTE number from the filename up to the first space
+    set "filename=%%~nF"
+    
+    REM Find the position of the first space and extract the SNOTE number
+    for /f "tokens=1*" %%s in ("!filename!") do (
+        set "snote=%%s"
+    )
+
+    REM Check if the folder exists; if not, create it
+    if not exist "!snote!\" (
+        mkdir "!snote!\"
+        echo [+] Created folder for TXT file: !snote!
+    )
+
+    echo.
+    echo Moving file: !txt_file! to folder: !snote!
+    move "!txt_file!" "!snote!\" >nul 2>&1
+            
+    if errorlevel 1 (
+        echo Failed to move: !txt_file! to !snote!
+    ) else (
+        echo Successfully moved: !txt_file! to !snote!
+    )
+    goto file_process_loop
 
 :url_processing
 cls
